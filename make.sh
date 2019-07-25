b3aa02e4586b614cc0d3f61591df18b9046c110f
root <root@ip-172-20-34-96.eu-west-1.compute.internal>
2019-07-25 08:51:29 +0000

    Base setup for WP for Kubernetes

---
+++ make.sh
@@ -0,0 +1,12 @@
+tr -d '\n' <password.txt >.strippedpassword.txt && mv .strippedpassword.txt password.txt
+
+# Install 
+kubectl create -f local-volumes.yaml
+kubectl create secret generic mysql-pass --from-file=password.txt
+kubectl create -f mysql-deployment.yaml
+kubectl create -f wordpress-deployment.yaml
+
+
+# Delete 
+kubectl delete secret mysql-pass
+kubectl delete deployment,service,pvc -l app=wordpress
